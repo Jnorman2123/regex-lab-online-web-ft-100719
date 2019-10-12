d2da8a26715fafc868539c356fc7e1c3da185d3f
Jonathan Norman <jnorman2123@gmail.com>
2019-10-12 13:38:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/^\W[0-9]{3}\W[0-9]{3}-[0-9]{4}$/)
+  if phone.match(/^\W{2}[0-9]{3}\W[0-9]{3}-[0-9]{4}\W$/)
     return true
   else
     return false

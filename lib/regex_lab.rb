f7319554762aaca3bf521a4bf208cfd743f5a2b7
Jonathan Norman <jnorman2123@gmail.com>
2019-10-12 12:51:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/^un.*ing$/)
+true_words = []
+  if text.scan(/^un.*ing$/)
     return true
   else
     return false

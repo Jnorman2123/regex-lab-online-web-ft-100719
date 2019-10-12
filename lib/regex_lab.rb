a1ea77c97778da6d80194a7f2fd2465fb4bc4e2c
Jonathan Norman <jnorman2123@gmail.com>
2019-10-12 14:11:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.each do |numbers|
-    p numbers.scan([0-9])
-  end
+  if phone.match(/^1?\(?\d{3}\)?\s?\d{3}\-?\s?\d{4}$/)
+    return true 
+  else 
+    return false 
+  end 
 end
